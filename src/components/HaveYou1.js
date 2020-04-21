68a1b36efbc4f31cf66107377134c23aebb68c73
Jack Dunning <JackStunning9001@gmail.com>
2020-04-21 11:38:26 -0700

    Add conditional rendering

---
+++ src/components/HaveYou1.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function HaveYou1(props){
+  return (
+    <React.Fragment>
+      <h3>Have you gone through all the steps on the Learn How to Program debugging lesson?</h3>
+    </React.Fragment>
+  );
+}
+
+export default HaveYou1;
--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -1,28 +1,48 @@
 import React from 'react';
 import NewTicketForm from './NewTicketForm';
 import TicketList from './TicketList';
+import HaveYou1 from './HaveYou1';
+import HaveYou2 from './HaveYou2';
+import HaveYou3 from './HaveYou3';
 
 class TicketControl extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      formVisibleOnPage: false
+      counter: 0
     };
   }
 
   handleClick = () => {
-    this.setState(prevState => ({
-      formVisibleOnPage: !prevState.formVisibleOnPage
-    }));
+    if(this.state.counter <= 3){
+      this.setState((state) => {
+        return {counter: state.counter + 1}
+      });
+    } else {
+      this.setState((state) => {
+        return {counter: 0}
+      });
+    }
+
+    console.log("hello")
   }
 
   render(){
     let currentlyVisibleState = null;
     let buttonText = null;
-    if (this.state.formVisibleOnPage) {
+    if (this.state.counter === 4) {
       currentlyVisibleState = <NewTicketForm />
       buttonText = "Return to Ticket List";
+    } else if (this.state.counter === 1) {
+      currentlyVisibleState = <HaveYou1 />
+      buttonText = "Yes";
+    } else if (this.state.counter === 2) {
+      currentlyVisibleState = <HaveYou2 />
+      buttonText = "Yes";
+    } else if (this.state.counter === 3) {
+      currentlyVisibleState = <HaveYou3 />
+      buttonText = "Yes";
     } else {
       currentlyVisibleState = <TicketList />
       buttonText = "Add Ticket";
