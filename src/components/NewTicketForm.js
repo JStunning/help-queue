812017e4ff1044a1ca234ad894a94f3abe2adcdd
Jack Dunning <JackStunning9001@gmail.com>
2020-04-27 10:59:04 -0700

    Add reusable form and details page

--- src/components/NewTicketForm.js
+++ src/components/NewTicketForm.js
@@ -1,9 +1,21 @@
 import React from "react";
+import { v4 } from 'uuid';
+import ReusableForm from "./ReusableForm";
 
 function NewTicketForm(props){
+
+  function handleNewTicketFormSubmission(event) {
+    event.preventDefault();
+    console.log(event.target.names.value);
+    console.log(event.target.location.value);
+    console.log(event.target.issue.value);
+  }
+  
   return (
     <React.Fragment>
-      <h3>This is a form.</h3>
+      <ReusableForm 
+        formSubmissionHandler={handleNewTicketFormSubmission}
+        buttonText="Help!" />
     </React.Fragment>
   );
 }
--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -1,58 +1,89 @@
 import React from 'react';
 import NewTicketForm from './NewTicketForm';
 import TicketList from './TicketList';
-import HaveYou1 from './HaveYou1';
-import HaveYou2 from './HaveYou2';
-import HaveYou3 from './HaveYou3';
+import TicketDetail from './TicketDetail';
+// import HaveYou1 from './HaveYou1';
+// import HaveYou2 from './HaveYou2';
+// import HaveYou3 from './HaveYou3';
 
 class TicketControl extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      counter: 0
+      formVisibleOnPage: false,
+      masterTicketList: [],
+      selectedTicket: null,
+      editing: false
     };
   }
 
   handleClick = () => {
-    if(this.state.counter <= 3){
-      this.setState((state) => {
-        return {counter: state.counter + 1}
+    if (this.state.selectedTicket != null) {
+      this.setState({
+        formVisibleOnPage: false,
+        selectedTicket: null
       });
     } else {
-      this.setState((state) => {
-        return {counter: 0}
-      });
+      this.setState(prevState => ({
+        formVisibleOnPage: !prevState.formVisibleOnPage,
+      }));
     }
+  }
 
-    console.log("hello")
+  handleAddingNewTicketToList = (newTicket) => {
+    const newMasterTicketList = this.state.masterTicketList.concat(newTicket);
+    this.setState({masterTicketList: newMasterTicketList});
+    this.setState({formVisibleOnPage: false});
+  }
+
+  handleChangingSelectedTicket = (id) => {
+    const selectedTicket = this.state.masterTicketList.filter(ticket => ticket.id === id)[0];
+    this.setState({selectedTicket: selectedTicket});
+  }
+
+  handleDeletingTicket = (id) => {
+    const newMasterTicketList = this.state.masterTicketList.filter(ticket => ticket.id !== id);
+    this.setState({
+      masterTicketList: newMasterTicketList,
+      selectedTicket: null
+    });
+  }
+
+  handleEditClick = () => {
+    console.log("handleEditClick reached!");
+    this.setState({editing: true});
   }
 
   render(){
     let currentlyVisibleState = null;
-    let buttonText = null;
-    if (this.state.counter === 4) {
-      currentlyVisibleState = <NewTicketForm />
+    let buttonText = null; 
+
+    if (this.state.selectedTicket != null) {
+      currentlyVisibleState = <TicketDetail 
+        ticket = {this.state.selectedTicket}
+        onClickingDelete = {this.handleDeletingTicket}
+        onClickingEdit = {this.handleEditClick} 
+      />
       buttonText = "Return to Ticket List";
-    } else if (this.state.counter === 1) {
-      currentlyVisibleState = <HaveYou1 />
-      buttonText = "Yes";
-    } else if (this.state.counter === 2) {
-      currentlyVisibleState = <HaveYou2 />
-      buttonText = "Yes";
-    } else if (this.state.counter === 3) {
-      currentlyVisibleState = <HaveYou3 />
-      buttonText = "Yes";
-    } else {
-      currentlyVisibleState = <TicketList />
-      buttonText = "Add Ticket";
+      // While our TicketDetail component only takes placeholder data, we will eventually be passing the value of selectedTicket as a prop.
     }
-    return (
-      <React.Fragment>
-        {currentlyVisibleState}
-        <button onClick={this.handleClick}>{buttonText}</button>
-      </React.Fragment>
-    );
+    else if (this.state.formVisibleOnPage) {
+      // This conditional needs to be updated to "else if."
+      currentlyVisibleState = <NewTicketForm onNewTicketCreation={this.handleAddingNewTicketToList}  />;
+      buttonText = "Return to Ticket List";
+    } else {
+      currentlyVisibleState = <TicketList ticketList={this.state.masterTicketList} onTicketSelection={this.handleChangingSelectedTicket} />;
+      // Because a user will actually be clicking on the ticket in the Ticket component, we will need to pass our new handleChangingSelectedTicket method as a prop.
+      buttonText = "Add Ticket";
+  }
+
+  return (
+    <React.Fragment>
+      {currentlyVisibleState}
+      <button onClick={this.handleClick}>{buttonText}</button>
+    </React.Fragment>
+  );
   }
 
 }
